53a94fc9c429738b53e346ebf3868c97874a3017
philipwilsonchang <philip.wilson.chang@gmail.com>
2016-12-20 16:29:51 -0500

    Added reset function to move hands back to 12:00:00

--- clock.js
+++ clock.js
@@ -9,6 +9,8 @@ radius = radius * 0.90;
 window.addEventListener('keydown', keyHandler, false);
 window.requestAnimationFrame(drawClock);
 
+// INTERACTIVE CODE
+
 function keyHandler(press) {
     var keyPressed = press.keyCode;
     switch (keyPressed) {
@@ -23,14 +25,22 @@ function pauseButton() {
         mode = "paused";
         window.requestAnimationFrame(drawClock);
     }
+    else {
+      mode = "clock";
+      window.requestAnimationFrame(drawClock);
+    }
 }
 
 function resetButton() {
-    if (mode != "clock") {
-        mode = "clock";
-        window.requestAnimationFrame(drawClock);
+    if (mode != "reset") {
+        mode = "reset";
+        window.requestAnimationFrame(resetClock);
     }
 }
+
+
+
+// DRAWING CODE
 
 function drawClock() {
 	drawFace(ctx, radius);
@@ -39,9 +49,43 @@ function drawClock() {
         currentHandPositions = drawHandsOnTime(ctx, radius);
         window.requestAnimationFrame(drawClock);
     }
+    else if (mode == "reset"){
+        currentHandPositions = drawHandsOnTime(ctx, radius);
+    }
     else {
         currentHandPositions = drawHandsOnTime(ctx, radius);
     }
+}
+
+function resetClock() {
+  // Draw clock face and numbers
+  drawFace(ctx, radius);
+  drawNumbers(ctx, radius);
+  var interHour = (0 + currentHandPositions[0]) / 1.06;
+  drawMinuteHand(ctx, interHour, radius * 0.5, radius * 0.07);
+  var interMinute = (0 + currentHandPositions[1]) / 1.06;
+  drawMinuteHand(ctx, interMinute, radius * 0.8, radius * 0.07);
+  var interSecond = (0 + currentHandPositions[2]) / 1.06;
+  drawSecondHand(ctx, interSecond, radius * 0.85, radius * 0.02);
+  // Redraw second hand central dot over hands
+  ctx.beginPath();
+  ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+
+  console.log(interHour + interMinute + interSecond);
+
+  if (interHour + interMinute + interSecond > 0.01) {
+    currentHandPositions = [interHour, interMinute, interSecond]
+    console.log("Resetting...");
+    window.requestAnimationFrame(resetClock);
+  }
+  else {
+    console.log("Completed reset");
+    mode = "paused";
+    window.requestAnimationFrame(drawClock);
+  }
+
 }
 
 function drawFace(ctx, radius) {
@@ -96,11 +140,12 @@ function drawNumbers(ctx, radius) {
 
 function drawHandsOnTime(ctx, radius) {
 	var now = new Date();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    var millisecond = now.getMilliseconds();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  var millisecond = now.getMilliseconds();
 
+  if (mode == "clock") {
     // Get hour hand position and draw
     hour = hour % 12;
     hour = (hour * Math.PI/6) + (minute * Math.PI/(6 * 60)) + (second * Math.PI/(360 * 60));
@@ -110,16 +155,31 @@ function drawHandsOnTime(ctx, radius) {
     drawMinuteHand(ctx, minute, radius * 0.8, radius * 0.07);
     // Get second hand position and draw
     second = (second * Math.PI/30) + (millisecond * Math.PI/(30 * 1000));
-    drawSecondHand(ctx, second, radius*0.85, radius*0.02);
+    drawSecondHand(ctx, second, radius * 0.85, radius * 0.02);
 
     // Redraw second hand central dot over hands
     ctx.beginPath();
-	ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
-	ctx.fillStyle = "red";
-	ctx.fill();
+  	ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
+  	ctx.fillStyle = "red";
+  	ctx.fill();
 
     // Return array of recently drawn positions
     return [hour, minute, second]
+  }
+  else {
+    drawMinuteHand(ctx, currentHandPositions[0], radius * 0.5, radius * 0.07);
+    drawMinuteHand(ctx, currentHandPositions[1], radius * 0.8, radius * 0.07);
+    drawSecondHand(ctx, currentHandPositions[2], radius * 0.85, radius * 0.02);
+
+    // Redraw second hand central dot over hands
+    ctx.beginPath();
+  	ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
+  	ctx.fillStyle = "red";
+  	ctx.fill();
+
+    // Return array of recently drawn positions
+    return currentHandPositions
+  }
 }
 
 
