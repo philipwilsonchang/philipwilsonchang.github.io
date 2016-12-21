4fbc789729edbaba1357aa41bfb3723b7930c13d
philipwilsonchang <philip.wilson.chang@gmail.com>
2016-12-21 10:26:37 -0500

    Restructured JS code to make drawing custom hands easier

--- clock.js
+++ clock.js
@@ -1,24 +1,15 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var radius = canvas.height / 2;
+var clockRadius = canvas.height / 2;
 var mode = "clock";
 var currentHandPositions = [0,0,0];
-ctx.translate(radius, radius);
-radius = radius * 0.90;
+ctx.translate(clockRadius, clockRadius);
+clockRadius = clockRadius * 0.90;
 
-window.addEventListener('keydown', keyHandler, false);
+// window.addEventListener('keydown', keyHandler, false);
 window.requestAnimationFrame(drawClock);
 
 // INTERACTIVE CODE
-
-function keyHandler(press) {
-    var keyPressed = press.keyCode;
-    switch (keyPressed) {
-        case 80: mode = "paused"; break;    // P
-        case 82: mode = "clock"; window.requestAnimationFrame(drawClock); break;    // R
-        default: mode = "clock"; window.requestAnimationFrame(drawClock);           // everything else
-    }
-}
 
 function pauseButton() {
     if (mode != "paused") {
@@ -32,9 +23,15 @@ function pauseButton() {
 }
 
 function zeroButton() {
-    if (mode != "reset") {
-        mode = "reset";
+    if (mode != "zeroed") {
+        mode = "zero";
+        document.getElementById('zeroButton').innerHTML = "Reset";
         window.requestAnimationFrame(zeroClock);
+    }
+    else if (mode == "zeroed") {
+      mode = "clock"
+      document.getElementById('zeroButton').innerHTML = "Zero";
+      window.requestAnimationFrame(drawClock);
     }
 }
 
@@ -43,35 +40,25 @@ function zeroButton() {
 // DRAWING CODE
 
 function drawClock() {
-	drawFace(ctx, radius);
-	drawNumbers(ctx, radius);
+	drawFace(ctx, clockRadius);
+	drawNumbers(ctx, clockRadius);
     if (mode == "clock") {
-        currentHandPositions = drawHandsOnTime(ctx, radius);
+        currentHandPositions = drawHandsOnTime(ctx, clockRadius);
         window.requestAnimationFrame(drawClock);
     }
-    else if (mode == "reset"){
-        currentHandPositions = drawHandsOnTime(ctx, radius);
-    }
     else {
-        currentHandPositions = drawHandsOnTime(ctx, radius);
+        drawHands(ctx, currentHandPositions, clockRadius);
     }
 }
 
 function zeroClock() {
   // Draw clock face and numbers
-  drawFace(ctx, radius);
-  drawNumbers(ctx, radius);
+  drawFace(ctx, clockRadius);
+  drawNumbers(ctx, clockRadius);
   var interHour = (0 + currentHandPositions[0]) / 1.06;
-  drawMinuteHand(ctx, interHour, radius * 0.5, radius * 0.07);
   var interMinute = (0 + currentHandPositions[1]) / 1.06;
-  drawMinuteHand(ctx, interMinute, radius * 0.8, radius * 0.07);
   var interSecond = (0 + currentHandPositions[2]) / 1.06;
-  drawSecondHand(ctx, interSecond, radius * 0.85, radius * 0.02);
-  // Redraw second hand central dot over hands
-  ctx.beginPath();
-  ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
-  ctx.fillStyle = "red";
-  ctx.fill();
+  drawHands(ctx, [interHour, interMinute, interSecond], clockRadius);
 
   console.log(interHour + interMinute + interSecond);
 
@@ -82,32 +69,32 @@ function zeroClock() {
   }
   else {
     console.log("Completed reset");
-    mode = "paused";
+    mode = "zeroed";
     window.requestAnimationFrame(drawClock);
   }
 
 }
 
-function drawFace(ctx, radius) {
+function drawFace(ctx, clockRadius) {
 	// Draw rim of clock
-	ctx.arc(0, 0, radius, 0, 2*Math.PI);
+	ctx.arc(0, 0, clockRadius, 0, 2*Math.PI);
 	ctx.fillStyle = "steelblue";
 	ctx.fill();
 
 	// Draw white face of clock over rim
 	ctx.beginPath();
-	ctx.arc(0, 0, radius*0.95, 0, 2*Math.PI);
+	ctx.arc(0, 0, clockRadius*0.95, 0, 2*Math.PI);
 	ctx.fillStyle = "white"
 	ctx.fill();
 
 	// Draw central dot of clock where hands attach
 	ctx.beginPath();
-	ctx.arc(0, 0, radius*0.075, 0, 2*Math.PI);
+	ctx.arc(0, 0, clockRadius*0.075, 0, 2*Math.PI);
 	ctx.fillStyle = "black";
 	ctx.fill();
 }
 
-function drawNumbers(ctx, radius) {
+function drawNumbers(ctx, clockRadius) {
 	var ang;
     var num;
     // Draw hour ticks
@@ -116,10 +103,10 @@ function drawNumbers(ctx, radius) {
         ctx.rotate(ang);
         ctx.strokeStyle = "black";
         ctx.beginPath();
-        ctx.lineWidth = radius * 0.03;
+        ctx.lineWidth = clockRadius * 0.03;
         ctx.lineCap = "round"
-        ctx.moveTo(0, radius * 0.80);
-        ctx.lineTo(0, radius * 0.90)
+        ctx.moveTo(0, clockRadius * 0.80);
+        ctx.lineTo(0, clockRadius * 0.90)
         ctx.stroke();
         ctx.rotate(-ang);
     }
@@ -129,57 +116,55 @@ function drawNumbers(ctx, radius) {
         ctx.rotate(ang);
         ctx.strokeStyle = "black";
         ctx.beginPath();
-        ctx.lineWidth = radius * 0.01;
+        ctx.lineWidth = clockRadius * 0.01;
         ctx.lineCap = "round"
-        ctx.moveTo(0, radius * 0.85);
-        ctx.lineTo(0, radius * 0.90);
+        ctx.moveTo(0, clockRadius * 0.85);
+        ctx.lineTo(0, clockRadius * 0.90);
         ctx.stroke();
         ctx.rotate(-ang);
     }
 }
 
-function drawHandsOnTime(ctx, radius) {
+// Draws hands at positions detailed by array handPositions ([hour, minute, second] in radians)
+function drawHands(ctx, handPositions, clockRadius) {
+  // Draw hour, minute, and second hands
+  drawMinuteHand(ctx, handPositions[0], clockRadius * 0.5, clockRadius * 0.07);
+  drawMinuteHand(ctx, handPositions[1], clockRadius * 0.8, clockRadius * 0.07);
+  drawSecondHand(ctx, handPositions[2], clockRadius * 0.85, clockRadius * 0.02);
+  // Redraw second hand central dot over hands
+  ctx.beginPath();
+  ctx.arc(0, 0, clockRadius*0.035, 0, 2*Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+}
+
+// Pulls system time and draws hands at correct positions, then returns their positions
+function drawHandsOnTime(ctx, clockRadius) {
 	var now = new Date();
   var hour = now.getHours();
   var minute = now.getMinutes();
   var second = now.getSeconds();
   var millisecond = now.getMilliseconds();
 
-  if (mode == "clock") {
-    // Get hour hand position and draw
-    hour = hour % 12;
-    hour = (hour * Math.PI/6) + (minute * Math.PI/(6 * 60)) + (second * Math.PI/(360 * 60));
-    drawMinuteHand(ctx, hour, radius * 0.5, radius * 0.07);
-    // Get minute hand position and draw
-    minute = (minute * Math.PI/30) + (second * Math.PI/(30 * 60));
-    drawMinuteHand(ctx, minute, radius * 0.8, radius * 0.07);
-    // Get second hand position and draw
-    second = (second * Math.PI/30) + (millisecond * Math.PI/(30 * 1000));
-    drawSecondHand(ctx, second, radius * 0.85, radius * 0.02);
+  // Get hour hand position and draw
+  hour = hour % 12;
+  hour = (hour * Math.PI/6) + (minute * Math.PI/(6 * 60)) + (second * Math.PI/(360 * 60));
+  drawMinuteHand(ctx, hour, clockRadius * 0.5, clockRadius * 0.07);
+  // Get minute hand position and draw
+  minute = (minute * Math.PI/30) + (second * Math.PI/(30 * 60));
+  drawMinuteHand(ctx, minute, clockRadius * 0.8, clockRadius * 0.07);
+  // Get second hand position and draw
+  second = (second * Math.PI/30) + (millisecond * Math.PI/(30 * 1000));
+  drawSecondHand(ctx, second, clockRadius * 0.85, clockRadius * 0.02);
 
-    // Redraw second hand central dot over hands
-    ctx.beginPath();
-  	ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
-  	ctx.fillStyle = "red";
-  	ctx.fill();
+  // Redraw second hand central dot over hands
+  ctx.beginPath();
+	ctx.arc(0, 0, clockRadius*0.035, 0, 2*Math.PI);
+	ctx.fillStyle = "red";
+	ctx.fill();
 
-    // Return array of recently drawn positions
-    return [hour, minute, second]
-  }
-  else {
-    drawMinuteHand(ctx, currentHandPositions[0], radius * 0.5, radius * 0.07);
-    drawMinuteHand(ctx, currentHandPositions[1], radius * 0.8, radius * 0.07);
-    drawSecondHand(ctx, currentHandPositions[2], radius * 0.85, radius * 0.02);
-
-    // Redraw second hand central dot over hands
-    ctx.beginPath();
-  	ctx.arc(0, 0, radius*0.035, 0, 2*Math.PI);
-  	ctx.fillStyle = "red";
-  	ctx.fill();
-
-    // Return array of recently drawn positions
-    return currentHandPositions
-  }
+  // Return array of recently drawn positions
+  return [hour, minute, second]
 }
 
 
