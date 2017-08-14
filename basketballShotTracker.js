03cf3d112bf365cb5ff4209946577ba5ccb6ec69
unknown <philip.wilson.chang@gmail.com>
2017-08-13 21:30:21 -0400

    Added shot display code

--- basketballShotTracker.js
+++ basketballShotTracker.js
@@ -2,8 +2,8 @@
 var totalMade = 0;
 var totalShot = 0;
 var percentage = 0.0;
-document.querySelector('.percentDisplay').innerHTML = percentage;
-document.querySelector('.fractionDisplay').innerHTML = totalMade + "/" + totalShot;
+document.getElementById('percentDisplay').innerHTML = percentage + "%";
+document.getElementById('fractionDisplay').innerHTML = totalMade + "/" + totalShot;
 
 function madeButton() {
   totalMade = totalMade + 1;
@@ -20,12 +20,12 @@ function zeroButton() {
   totalMade = 0;
   totalShot = 0;
   percentage = 0.0;
-  document.querySelector('.percentDisplay').innerHTML = percentage;
-  document.querySelector('.fractionDisplay').innerHTML = totalMade + "/" + totalShot;
+  document.getElementById('percentDisplay').innerHTML = percentage + "%";
+  document.getElementById('fractionDisplay').innerHTML = totalMade + "/" + totalShot;
 }
 
 function recalculate() {
   percentage = (totalMade / totalShot * 100.0).toFixed(1);
-  document.querySelector('.percentDisplay').innerHTML = percentage;
-  document.querySelector('.fractionDisplay').innerHTML = totalMade + "/" + totalShot;
+  document.getElementById('percentDisplay').innerHTML = percentage + "%";
+  document.getElementById('fractionDisplay').innerHTML = totalMade + "/" + totalShot;
 }
